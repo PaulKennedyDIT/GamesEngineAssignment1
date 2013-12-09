42a1e26b4d835dd81b0a02ec9e63b0768e0e729f
PaulKennedyDIT <c10319445@mydit.ie>
2013-12-09 15:12:55 +0000

    Add Assignment

---
+++ BGE/Assignment.cpp
@@ -0,0 +1,74 @@
+#include "Assignment.h"
+#include "PhysicsController.h"
+#include "Sphere.h"
+#include "PhysicsCamera.h"
+#include "Box.h"
+#include "Cylinder.h"
+#include "Steerable3DController.h"
+#include "Ground.h"
+#include "Content.h"
+#include <btBulletDynamicsCommon.h>
+#include <gtc/quaternion.hpp>
+#include <gtx/quaternion.hpp>
+#include <gtx/euler_angles.hpp>
+#include <gtx/norm.hpp>
+#include "VectorDrawer.h"
+#include "Utils.h"
+
+using namespace BGE;
+
+Assignment::Assignment(void)
+{
+	physicsFactory = NULL;
+	dynamicsWorld = NULL;
+	broadphase = NULL;
+	dispatcher = NULL;
+	solver = NULL;
+	fullscreen = false;
+
+}
+
+Assignment::~Assignment(void)
+{
+}
+
+bool Assignment::Initialise() 
+{
+	riftEnabled = false;
+	// Set up the collision configuration and dispatcher
+    collisionConfiguration = new btDefaultCollisionConfiguration();
+    dispatcher = new btCollisionDispatcher(collisionConfiguration);
+ 
+    // The world.
+	btVector3 worldMin(-1000,-1000,-1000);
+	btVector3 worldMax(1000,1000,1000);
+	broadphase = new btAxisSweep3(worldMin,worldMax);
+	solver = new btSequentialImpulseConstraintSolver();
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+    dynamicsWorld->setGravity(btVector3(0,0,0));
+
+
+	physicsFactory = make_shared<PhysicsFactory>(dynamicsWorld);
+
+	physicsFactory->CreateGroundPhysics();
+	physicsFactory->CreateCameraPhysics();
+
+	if (!Game::Initialise()) {
+		return false;
+	}
+
+	camera->GetController()->position = glm::vec3(0,10, 20);
+	
+	return true;
+}
+
+void BGE::Assignment::Update(float timeDelta)
+{
+	dynamicsWorld->stepSimulation(timeDelta,100);
+	Game::Update(timeDelta);
+}
+
+void BGE::Assignment::Cleanup()
+{
+	Game::Cleanup();
+}
